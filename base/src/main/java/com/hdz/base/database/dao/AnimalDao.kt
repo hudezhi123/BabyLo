d67be59797e18cf194e4c7d3a502c04887abd619
hdz <18401541305@163.com>
2021-09-17 14:27:07 +0800

    主要更新图片资源

--- base/src/main/java/com/hdz/base/database/dao/AnimalDao.kt
+++ base/src/main/java/com/hdz/base/database/dao/AnimalDao.kt
@@ -1,4 +1,17 @@
 package com.hdz.base.database.dao
 
-class AnimalDao {
+import androidx.room.Dao
+import androidx.room.Query
+import com.hdz.base.database.BaseDao
+import com.hdz.base.database.entity.Animal
+
+@Dao
+interface AnimalDao : BaseDao<Animal> {
+
+    @Query("select * from Animal")
+    fun getAllAnimals(): MutableList<Animal>
+
+    @Query("select * from Animal where name = :animalName")
+    fun getAnimal(animalName: String):Animal
+
 }
--- base/src/main/java/com/hdz/base/database/dao/PlantDao.kt
+++ base/src/main/java/com/hdz/base/database/dao/PlantDao.kt
@@ -1,4 +1,16 @@
 package com.hdz.base.database.dao
 
-class PlantDao {
+import androidx.room.Dao
+import androidx.room.Query
+import com.hdz.base.database.BaseDao
+import com.hdz.base.database.entity.Plant
+
+@Dao
+interface PlantDao : BaseDao<Plant> {
+
+    @Query("select * from Plant")
+    fun getAllPlants(): MutableList<Plant>
+
+    @Query("select * from Plant where name = :plantName")
+    fun getPlant(plantName:String):Plant
 }
