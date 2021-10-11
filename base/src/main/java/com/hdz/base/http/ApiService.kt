37f743fd04b53271b2fa8ae0a6383f528faeecb9
hdz <18401541305@163.com>
2021-10-11 13:44:25 +0800

    更新json数据,添加图片

--- base/src/main/java/com/hdz/base/http/ApiService.kt
+++ base/src/main/java/com/hdz/base/http/ApiService.kt
@@ -6,6 +6,12 @@ import com.hdz.base.http.bean.ListBean
 
 interface ApiService {
 
+    companion object{
+        val BASE_URL = "http://192.168.2.84:8080/BabyLo/"
+    }
+
+
+
     suspend fun getAnimalList(): Data<ListBean<Animal>>
 
     suspend fun getPlantList(): Data<ListBean<Plant>>
