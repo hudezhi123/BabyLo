fd3eac2cdf7eb21ee627b9ec156ba531e5bd0551
hdz <18401541305@163.com>
2021-12-09 10:17:59 +0800

    Main

--- app/src/main/java/com/hdz/babylo/MainActivity.kt
+++ app/src/main/java/com/hdz/babylo/MainActivity.kt
@@ -2,11 +2,15 @@ package com.hdz.babylo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mTitle: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mTitle = TextView(this)
     }
 }
